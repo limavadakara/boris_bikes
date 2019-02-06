9f798c7504f49d5aed905641d0f49756d3e57c01
“Brenda” <odonnell.brenda@gmail.com>
2019-02-06 15:46:26 +0000

    changed name of working method to working? method

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -2,7 +2,7 @@ require 'bike'
 describe Bike do
   bike = Bike.new
   it 'should respond to method working' do
-    expect(bike).to respond_to(:working)
+    expect(bike).to respond_to(:working?)
   end
 
 end
