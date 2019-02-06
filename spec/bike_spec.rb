f904cbf76689f5f641293ba3f801a8e903c25a56
“Brenda” <odonnell.brenda@gmail.com>
2019-02-06 17:06:58 +0000

    Refactored bike_spec to use subject

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,12 +1,11 @@
 require 'bike'
 describe Bike do
-  bike = Bike.new
   it 'should respond to method working' do
-    expect(bike).to respond_to(:working?)
+    expect(subject).to respond_to(:working?)
   end
 
   it 'should return yes for working?' do
-    expect(bike.working?).to eq true
+    expect(subject.working?).to eq true
   end
 
 end
