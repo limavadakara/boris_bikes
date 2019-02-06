8a995b7755dd28fe7bda44e32666b6fc5a36040c
Lima Vadakara <lima.vadakara@gmail.com>
2019-02-06 14:53:41 +0000

    Passed test for dockingstation respond_to release_bike

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -1,4 +1,7 @@
 require 'docking_station'
 describe DockingStation do
-
+  it 'should respond to release_bike command' do
+    dockingstation = DockingStation.new
+    expect(dockingstation).to respond_to(:release_bike)
+  end
 end
