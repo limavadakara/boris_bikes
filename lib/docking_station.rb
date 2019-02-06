e278d29c4cd92905748d2e5d0875bfa9597fb420
“Brenda” <odonnell.brenda@gmail.com>
2019-02-06 16:21:13 +0000

    created test case for release_bike and fixed error by updating method to return bike

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,6 +1,7 @@
 class DockingStation
   def release_bike
-  
+    bike = Bike.new
+    return bike 
   end
 
 end
--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -4,4 +4,9 @@ describe DockingStation do
   it 'should respond to release_bike command' do
     expect(dockingstation).to respond_to(:release_bike)
   end
+
+
+  it 'should return a bike when asked to release a bike' do
+    expect(dockingstation.release_bike).to be_a Bike  
+  end
 end
